22f604489b222969061a87c76e99b94953504692
Brian Bissette <bbissette3@yahoo.com>
2023-05-15 19:50:10 -0500

    corrected typo in dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,14 +1,14 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
-const pool = new Pool ({
-    user: 'postgres',
-    db: 'study-sphere-db',
-    password: 'postgres',
-    port: 5432,
-    host: 'localhost',
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 20000,
-    min: 0
-})
+const pool = new Pool({
+  user: "postgres",
+  db: "study-sphere-db",
+  password: "postgres",
+  port: 5432,
+  host: "localhost",
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 20000,
+  min: 0,
+});
 
-module.exports = { Pool }
+module.exports = { pool };
